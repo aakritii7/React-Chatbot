441a725cf3a7ac9715488b888857cdda04dc026d
Kidddooo <mahatoaakriti3@gmail.com>
2026-06-30 14:01:12 +0545

    Styling the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import ChatbotIcon from './components/ChatbotIcon'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='container'>
+      <div className="chatbot-popup">
+
+
+        {/* Chatbot Header */}
+        <div className="chat-header">
+          <div className="header-info">
+            <ChatbotIcon />
+            <h2 className="logo-text">Chatbot</h2>
+          </div>
+          <button class="material-symbols-outlined">
+            keyboard_arrow_down
+          </button>
+        </div>
+
+        {/* Chatbot Body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+            <ChatbotIcon />
+            <p className='message-text'>Hey there 👋 <br /> How can I help you today?</p>
+          </div>
+          <div className="message user-message">
+            <ChatbotIcon />
+            <p className='message-text'>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+          </div>
+
+        </div>
+
+         {/*Chatbot Footer  */}
+        <div className="chat-footer">
+          <form action="#" className="chat-form">
+            <input type="text" placeholder='Message...' className='message-input' required />
+            <button class="material-symbols-outlined">
+            arrow_upward
+          </button>
+          </form>
+
+        </div>
+      </div>
     </div>
   )
 }
